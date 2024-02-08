c3535e3fba4e1200f678d564344597d98ec7ad34
Nicola-Gozhankine <nikgorozhankin@gmail.com>
2024-02-08 12:12:28 +0300

    Основа  програмы 2

--- GitL3/Program.cs
+++ GitL3/Program.cs
@@ -8,8 +8,12 @@ namespace GitL3
 {
     internal class Program
     {
-        static void Main(string[] args)
+
+        static void Main()
         {
+            double x = 1.5;  // Значение x, для которого нужно вычислить функцию
+            double result = 8 * Math.Sin(2 * x) - Math.Cos(2 * x);
+            Console.WriteLine("Результат функции 8sin(2x) - cos(2x) при x = " + x + " равен " + result);
 
         }
     }
