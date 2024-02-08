688eba05f2af0e50652a28538d5760828ba6685c
Nicola-Gozhankine <nikgorozhankin@gmail.com>
2024-02-08 12:29:08 +0300

    Окончательная версия

--- GitL3/Program.cs
+++ GitL3/Program.cs
@@ -11,6 +11,7 @@ namespace GitL3
 
         static void Main()
         {
+            Console.WriteLine("Ведите значение параметра х "); 
             double x = Convert.ToDouble(Console.ReadLine());  // Значение x, для которого нужно вычислить функцию
             double result = 8 * Math.Sin(2 * x) - Math.Cos(2 * x);
             Console.WriteLine("Результат функции 8sin(2x) - cos(2x) при x = " + x + " равен " + result);
