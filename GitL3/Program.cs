354878e3c08cfb89a675e9a2504b0e930aa4514a
Nicola-Gozhankine <nikgorozhankin@gmail.com>
2024-02-08 12:24:16 +0300

    Итого 1

--- GitL3/Program.cs
+++ GitL3/Program.cs
@@ -11,10 +11,10 @@ namespace GitL3
 
         static void Main()
         {
-            double x = 1.5;  // Значение x, для которого нужно вычислить функцию
+            double x = Convert.ToDouble(Console.ReadLine());  // Значение x, для которого нужно вычислить функцию
             double result = 8 * Math.Sin(2 * x) - Math.Cos(2 * x);
             Console.WriteLine("Результат функции 8sin(2x) - cos(2x) при x = " + x + " равен " + result);
-
+            Console.ReadKey();
 
         }
     }
